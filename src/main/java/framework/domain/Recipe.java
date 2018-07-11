a18044e77cfc5ed155bb15f8b749359bb4667bd0
chunwai lai <peter009@yahoo.com>
2018-07-11 15:24:53 +0000

    Initial Check in Add Notes <-> Recipe one to one relationship Change package name

---
+++ src/main/java/framework/domain/Recipe.java
@@ -0,0 +1,100 @@
+package framework.domain;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Recipe {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String description;
+    private String prepTime;
+    private String cookTime;
+    private String servings;
+    private String source;
+    private String url;
+    private String direction;
+    //todo add
+    //private Difficulty difficulty;
+    
+    @Lob  //BLOB to store image
+    private byte[] image;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public String getPrepTime() {
+        return prepTime;
+    }
+    
+    public void setPrepTime(String prepTime) {
+        this.prepTime = prepTime;
+    }
+    
+    public String getCookTime() {
+        return cookTime;
+    }
+    
+    public void setCookTime(String cookTime) {
+        this.cookTime = cookTime;
+    }
+    
+    public String getServings() {
+        return servings;
+    }
+    
+    public void setServings(String servings) {
+        this.servings = servings;
+    }
+    
+    public String getSource() {
+        return source;
+    }
+    
+    public void setSource(String source) {
+        this.source = source;
+    }
+    
+    public String getUrl() {
+        return url;
+    }
+    
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    public String getDirection() {
+        return direction;
+    }
+    
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+    
+    public Notes getNotes() {
+        return notes;
+    }
+    
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+    
+    public byte[] getImage() {
+        return image;
+    }
+    
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
--- src/test/java/framework/Spring5RecipeAppApplicationTests.java
+++ src/test/java/framework/Spring5RecipeAppApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework;
+package framework;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
